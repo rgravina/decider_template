ec32957fed1f2c107fb2bf3ffdda2cc94e23cb1e
Chiharu Miya <miya.chiharu@gmail.com>
2022-01-31 15:52:57 +0900

    method object pattern

--- rps/src/decider.js
+++ rps/src/decider.js
@@ -7,31 +7,43 @@ export const Throws = {
 const ThrowPattern = [Throws.rock, Throws.scissors, Throws.paper]
 export default class Requests {
     play = (p1, p2, observer) => {
-        if (this.invalidInput(p1, p2)) {
-            observer.invalidInput()
+        new PlayRroundRequest(observer, p1, p2).play()
+    }
+}
+
+class PlayRroundRequest {
+    constructor(observer, p1, p2) {
+       this.p1 = p1
+       this.p2 = p2
+       this.observer = observer
+    }
+
+    play = () => {
+        if (this.invalidInput()) {
+            this.observer.invalidInput()
             return
         }
         
-        if (this.tie(p1, p2)) {
-            observer.tie()
-        } else if (this.p1Wins(p1, p2)) {
-            observer.p1Wins()
+        if (this.tie()) {
+            this.observer.tie()
+        } else if (this.p1Wins()) {
+            this.observer.p1Wins()
         } else {
-            observer.p2Wins()
+            this.observer.p2Wins()
         }
     }
 
-    invalidInput = (p1, p2) => {
-        return !(ThrowPattern.includes(p1) && ThrowPattern.includes(p2))
+    invalidInput = () => {
+        return !(ThrowPattern.includes(this.p1) && ThrowPattern.includes(this.p2))
     }
 
-    tie = (p1, p2) => {
-        return p1 === p2
+    tie = () => {
+        return this.p1 === this.p2
     }
     
-    p1Wins = (p1, p2) => {
-        return p1 === Throws.rock && p2 === Throws.scissors || 
-        p1 === Throws.scissors && p2 === Throws.paper || 
-        p1 === Throws.paper && p2 === Throws.rock
+    p1Wins = () => {
+        return this.p1 === Throws.rock && this.p2 === Throws.scissors || 
+        this.p1 === Throws.scissors && this.p2 === Throws.paper || 
+        this.p1 === Throws.paper && this.p2 === Throws.rock
     }
 }
