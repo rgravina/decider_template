3eaedd0fcdd9910ae233420ca1c6ef25c8772a38
Chiharu Miya <miya.chiharu@gmail.com>
2022-01-31 12:13:29 +0900

    リファクタリング

--- rps/src/decider.js
+++ rps/src/decider.js
@@ -7,19 +7,31 @@ export const Throws = {
 const ThrowPattern = [Throws.rock, Throws.scissors, Throws.paper]
 export default class Requests {
     play = (p1, p2, observer) => {
-        if (!(ThrowPattern.includes(p1) && ThrowPattern.includes(p2))) {
+        if (this.invalidInput(p1, p2)) {
             observer.invalidInput()
             return
         }
         
-        if (p1 === p2) {
+        if (this.tie(p1, p2)) {
             observer.tie()
-        } else if (p1 === Throws.rock && p2 === Throws.scissors || 
-            p1 === Throws.scissors && p2 === Throws.paper || 
-            p1 === Throws.paper && p2 === Throws.rock) {
+        } else if (this.p1Wins(p1, p2)) {
             observer.p1Wins()
         } else {
             observer.p2Wins()
         }
-    }    
+    }
+
+    invalidInput = (p1, p2) => {
+        return !(ThrowPattern.includes(p1) && ThrowPattern.includes(p2))
+    }
+
+    tie = (p1, p2) => {
+        return p1 === p2
+    }
+    
+    p1Wins = (p1, p2) => {
+        return p1 === Throws.rock && p2 === Throws.scissors || 
+        p1 === Throws.scissors && p2 === Throws.paper || 
+        p1 === Throws.paper && p2 === Throws.rock
+    }
 }
