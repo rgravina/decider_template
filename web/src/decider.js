a3294a2b5d1a89d38b77d5e04a9be1f6063542fb
Chiharu Miya <miya.chiharu@gmail.com>
2022-02-09 15:26:32 +0900

    入力を受け付けるところまで実装

--- web/src/decider.js
+++ web/src/decider.js
@@ -9,7 +9,7 @@ export default class Decider extends React.Component {
     }
 
     submit() {
-        this.props.requests.play("", "", this)
+        this.props.requests.play(this.state.p1, this.state.p2, this)
     }
 
     invalidInput() {
@@ -28,10 +28,20 @@ export default class Decider extends React.Component {
         this.setState({result: "Player 2 Wins!"})
     }
 
+    onP1Change(event) {
+        this.setState({p1: event.target.value})
+    }
+
+    onP2Change(event) {
+        this.setState({p2: event.target.value})
+    }
+
     render () {
         return (
             <div>
                 <p>{this.state.result}</p>
+                <input name="p1" onChange={this.onP1Change.bind(this)} />
+                <input name="p2" onChange={this.onP2Change.bind(this)} />
                 <button onClick={this.submit.bind(this)}>PLAY</button>
             </div>
         )
